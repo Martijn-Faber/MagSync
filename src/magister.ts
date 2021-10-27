1e31c3d5ce405b196e128fd8a2473d3f36c4af35
Martijn-Faber <martijn@fabtronics.nl>
2021-10-28 00:44:56 +0200

    feat(magister): add token refreshing

--- src/magister.ts
+++ src/magister.ts
@@ -2,14 +2,17 @@ import { config } from './util/config';
 import { AuthManager } from 'magister-openid';
 import Logger from './util/logger';
 import Account from './interfaces/account';
-import fetch from 'node-fetch';
+import fetch, { RequestInfo, RequestInit } from 'node-fetch';
 import { Appointment } from './interfaces';
+import { URLSearchParams } from 'url';
+import dayjs from 'dayjs';
 
 export class Magister {
   private logger: Logger;
 
-  private accesToken: string;
+  private accessToken: string;
   private refreshToken: string;
+  private tokenExpire: dayjs.Dayjs;
 
   private tenant: string;
 
@@ -26,54 +29,88 @@ export class Magister {
     );
 
     const tokens = await manager.login(username, password, authCode);
-    this.accesToken = tokens.access_token;
-    // TODO: refresh token
     this.refreshToken = tokens.refresh_token;
+    this.accessToken = tokens.access_token;
+    this.tokenExpire = dayjs().add(tokens.expires_in, 'seconds');
+  }
+
+  async refreshAccessToken(): Promise<string> {
+    const res = await fetch('https://accounts.magister.net/connect/token', {
+      method: 'POST',
+      headers: {
+        'cache-control': 'no-cache',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        refresh_token: this.refreshToken,
+        client_id: 'M6LOAPP',
+        grant_type: 'refresh_token'
+      })
+    }).then((res) => res.json());
+
+    this.refreshToken = res.refresh_token;
+    this.accessToken = res.access_token;
+    this.tokenExpire = dayjs().add(res.expires_in, 'seconds');
+
+    return res.access_token;
+  }
+
+  private async handleAuth(): Promise<string> {
+    if (this.accessToken) {
+      // check if token is expired
+      if (dayjs() >= this.tokenExpire) {
+        return await this.refreshAccessToken(); // refresh token
+      }
+
+      return this.accessToken;
+    }
+  }
+
+  private async fetch(url: RequestInfo, init?: RequestInit): Promise<any> {
+    init = {
+      ...init,
+      headers: {
+        Authorization: `Bearer ${await this.handleAuth()}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        ...init?.headers
+      }
+    };
+
+    let res = await fetch(url, init);
+
+    if (res.status == 401) {
+      this.handleAuth();
+    }
+
+    if (res.status !== 200) {
+      this.logger.log(`request failed with a ${res.status} ${res.statusText}`);
+    }
+
+    return await res.json();
   }
 
   async account(): Promise<Account> {
     this.logger.log('fetching Magister account');
 
-    const res = await fetch(`https://${config.tenant}/api/account?noCache=0`, {
-      headers: {
-        Authorization: `Bearer ${this.accesToken}`
-      }
-    });
-
-    if (!res.ok) {
-      this.logger.error('fetching account failed');
-      return;
-    }
+    const account = await this.fetch(`https://${config.tenant}/api/account?noCache=0`);
 
     this.logger.log('successfully fetched account');
-
-    return await res.json();
+    return await account;
   }
 
   async appointments(from: string, to: string): Promise<Appointment[]> {
     this.logger.log('fetching Magister appointments');
 
-    const res = await fetch(
+    const appointments = await this.fetch(
       `https://${this.tenant}/api/personen/${
         (
           await this.account()
         ).Persoon.Id
-      }/afspraken?status=1&tot=${to}&van=${from}`,
-      {
-        headers: {
-          Authorization: `Bearer ${this.accesToken}`
-        }
-      }
+      }/afspraken?status=1&tot=${to}&van=${from}`
     );
 
-    if (!res.ok) {
-      this.logger.error('fetching appointments failed');
-      return;
-    }
-
     this.logger.log('successfully fetched appointments');
-
-    const json = await res.json();
-    return json.Items;
+    return appointments.Items;
   }
 }
